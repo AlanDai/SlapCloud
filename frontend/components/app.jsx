f333d1a8e10e26e9933c427ee64818d33ab88233
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-09 20:34:42 -0800

    add routing logic and a route that only renders if not logged in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import { AuthRoute } from '../util.route_util';
+import { AuthRoute } from '../util/route_util';
 import LandingPage from "./landing_page";
 
 const App = () => (
   <div>
     <Switch>
       <AuthRoute exact path="/" component={LandingPage} />
-      <Route path="/" render={<h1>Discover</h1>}/>
+      <Route path="/" render={()=><h1>Discover</h1>}/>
     </Switch>
   </div>
 )
-
 
 export default App;
