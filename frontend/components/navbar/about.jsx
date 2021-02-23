47b852bf083c356dc8a36b984c2126c557b543f8
alanxudai@gmail.com <alanxudai@gmail.com>
2021-02-23 15:15:02 -0800

    Make about links open new tab

--- frontend/components/navbar/about.jsx
+++ frontend/components/navbar/about.jsx
@@ -5,8 +5,12 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const About = (props) => (
   <div id="about-sidebar">
-    <a href="https://github.com/AlanDai/SlapCloud"><FontAwesomeIcon icon={faGithub} /></a>
-    <a href="https://www.linkedin.com/in/alanxudai/"><FontAwesomeIcon icon={faLinkedin} /></a>
+    <a href="https://github.com/AlanDai/SlapCloud" target="_blank" rel="no opener noreferrer">
+      <FontAwesomeIcon icon={faGithub} />
+    </a>
+    <a href="https://www.linkedin.com/in/alanxudai/" target="_blank" rel="no opener noreferrer">
+      <FontAwesomeIcon icon={faLinkedin} />
+    </a>
   </div>
 )
 
