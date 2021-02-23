dfeef1589bac16f6cac56f6d27951eddace2ee4f
alanxudai@gmail.com <alanxudai@gmail.com>
2021-02-23 14:38:14 -0800

    Limited the size of the discover page carousel

--- frontend/components/slaps/slap_index.jsx
+++ frontend/components/slaps/slap_index.jsx
@@ -17,19 +17,19 @@ class SlapsIndex extends React.Component {
         { Object.keys(slaps).length &&
           <div className="slap-index-section">
             <div className="carousel-header">Featured Song</div>
-            <div className="carousel-subheader">Check out this slap, fresh off the press!</div>
+            <div className="carousel-subheader">Check out the latest slap, fresh off the press!</div>
             <SlapItemContainer slap={slaps[Object.keys(slaps)[Object.keys(slaps).length - 1]]}/>
           </div>
         }
         <div className="slap-index-section">
           <div className="carousel-header">More of what you like</div>
           <div className="carousel-subheader">Suggestions based on what you've liked or played</div>
-          <SlapsCarousel slaps={Object.values(slaps)} />
+          <SlapsCarousel slaps={Object.values(slaps).slice(0, 10)} />
         </div>
         <div className="slap-index-section">
           <div className="carousel-header">Discover new music</div>
           <div className="carousel-subheader">A compilation of our picks of the week</div>
-          <SlapsCarousel slaps={Object.values(slaps).reverse()} />
+          <SlapsCarousel slaps={Object.values(slaps).reverse().slice(0, 10)} />
         </div>
       </div>
     )
